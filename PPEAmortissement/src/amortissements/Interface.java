2d2e5b3079642af63074d95dd261afff6434189a
Jean-baptiste <jeanbaptiste.gautier7@gmail.com>
2015-11-09 22:40:55 +0100

    Commencement Interface graphique(JB)

---
+++ PPEAmortissement/src/amortissements/Interface.java
@@ -0,0 +1,16 @@
+package amortissements;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Interface {
+	public static void main(String[] args)
+	{
+		JFrame fenetre = new JFrame();
+		fenetre.setTitle("Interface");
+		fenetre.setSize(400, 100);
+		fenetre.setLocationRelativeTo(null);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setVisible(true);
+	}
+
+}
