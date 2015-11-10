ffb01a193240da96cefbe1dde46ff18a082064e8
Jean-baptiste <jeanbaptiste.gautier7@gmail.com>
2015-11-10 14:41:35 +0100

    2ème essaie interface (Jb)

--- PPEAmortissement/src/amortissements/Interface.java
+++ PPEAmortissement/src/amortissements/Interface.java
@@ -1,16 +1,20 @@
 package amortissements;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Interface {
-	public static void main(String[] args)
-	{
+import javax.swing.*;
+
+public class Interface{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
 		JFrame fenetre = new JFrame();
-		fenetre.setTitle("Interface");
-		fenetre.setSize(400, 100);
-		fenetre.setLocationRelativeTo(null);
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setTitle("Interface graphique");
+		fenetre.setSize(300, 200);
+		
+		JLabel label = new JLabel("Bonjour");
+		fenetre.getContentPane().add(label);
+		
 		fenetre.setVisible(true);
 	}
-
 }
