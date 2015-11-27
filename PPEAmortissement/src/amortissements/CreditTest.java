552f8308b15677a8ff555dec6bce02281c245828
Jean-baptiste <jeanbaptiste.gautier7@gmail.com>
2015-11-27 11:41:27 +0100

    TestFinit

--- PPEAmortissement/src/amortissements/CreditTest.java
+++ PPEAmortissement/src/amortissements/CreditTest.java
@@ -10,38 +10,38 @@ public class CreditTest {
 	public void testCalculeTaux() {
 		
 		double taux = 3;
-		assertEquals(taux,Credit.calculeTaux(1, 20000, 3100, 8).taux(),0.1);
+		assertEquals(taux,Credit.calculeTaux(Credit.AMORTISSEMENT_CONSTANTS, 20000, 3100, 8).taux(),0.1);
 		
 		/*double taux1 = 3;
-		assertEquals(taux1,Credit.calculeTaux(2, 20000, 2849, 8).taux(),0);*/
+		assertEquals(taux1,Credit.calculeTaux(Credit.ANNUITES_CONSTANTES, 20000, 2849, 8).taux(),0);*/
 	}
 
 	@Test
 	public void testCalculeDuree() {
 		int duree = 8;
-		assertEquals(duree,Credit.calculeDuree(1, 20000, 3100, 3).duree(),0.1);
+		assertEquals(duree,Credit.calculeDuree(Credit.AMORTISSEMENT_CONSTANTS, 20000, 3100, 3).duree(),0.1);
 		
 		/*int duree1 = 8;
-		assertEquals(duree1,Credit.calculeDuree(2, 20000, 2849, 3).duree(),0);*/
+		assertEquals(duree1,Credit.calculeDuree(Credit.ANNUITES_CONSTANTES, 20000, 2849, 3).duree(),0);*/
 
 	}
 
 	@Test
-	public void testCalculeMontantEmprunte() {
+	public void testCalculeMontantEmprunte() { 
 		double montant = 20000;
-		assertEquals(montant,Credit.calculeMontantEmprunte(1, 3100, 3, 8).montantEmprunte(),0.1);
+		assertEquals(montant,Credit.calculeMontantEmprunte(Credit.AMORTISSEMENT_CONSTANTS, 3100, 3, 8).montantEmprunte(),0.1);
 		
 		/*double montant1 = 20000;
-		assertEquals(montant1,Credit.calculeMontantEmprunte(2, 2849, 3, 8).montantEmprunte(),0.1);*/
+		assertEquals(montant1,Credit.calculeMontantEmprunte(Credit.ANNUITES_CONSTANTES, 2849, 3, 8).montantEmprunte(),0.1);*/
 	}
 
 	@Test
 	public void testCalculeAnnuiteMaximale() {
 		double annuiteMax = 3100;
-		assertEquals(annuiteMax,Credit.calculeAnuiteMaximale(1, 20000, 3, 8).annuiteMaximale(),0.1);
+		assertEquals(annuiteMax,Credit.calculeAnuiteMaximale(Credit.AMORTISSEMENT_CONSTANTS, 20000, 3, 8).annuiteMaximale(),0.1);
 		
 		/*double annuiteMax1 = 2849;
-		assertEquals(annuiteMax1,Credit.calculeAnnuiteMaximale(2, 20000, 3, 8).annuiteMaximale(),0.1);*/
+		assertEquals(annuiteMax1,Credit.calculeAnnuiteMaximale(Credit.ANNUITES_CONSTANTES, 20000, 3, 8).annuiteMaximale(),0.1);*/
 
 	}
 }
